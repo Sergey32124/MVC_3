86b53f48b92930dcc71b95bd5400752d19437504
Sergey12432 <tchuxin201@yandex.ru>
2024-10-07 12:45:52 +0300

    Initial commit

---
+++ src/main/java/org/example/mvc_3/Mvc3Application.java
@@ -0,0 +1,13 @@
+package org.example.mvc_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvc3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mvc_3/Mvc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mvc_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
